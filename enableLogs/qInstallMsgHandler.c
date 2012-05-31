260e8ef46de8717fb59f1cedd3f226803d41d6f9
Denis Mingulov <denis.mingulov@gmail.com>
2012-05-31 10:02:02 +0300

    Initial version for qInstallMsgHandler

---
+++ enableLogs/qInstallMsgHandler.c
@@ -0,0 +1,9 @@
+
+
+#define qInstallMsgHandler _Z18qInstallMsgHandlerPFv9QtMsgTypePKcE
+
+// void *qInstallMsgHandler(void *p)
+void *_Z18qInstallMsgHandlerPFv9QtMsgTypePKcE (void *p)
+{
+    return 0;
+}
